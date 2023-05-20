4a15d69d24dca313e4941ede3667bf88de43d636
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-05-20 12:39:15 +0800

    feature:use byte block as easyocr input

--- easyocr/webservice.py
+++ easyocr/webservice.py
@@ -1,19 +1,15 @@
 # 对easyocr提供接口
-import os
-import io
+# import os
+# PROJECT_DIR= os.path.dirname(
+#         os.path.dirname(os.path.realpath( __file__))
+# )
 from flask import Flask, render_template, request,jsonify
-from werkzeug import security
+import json
 import easyocr
 import json
-from PIL import Image 
 import numpy as np
-PROJECT_DIR= os.path.dirname(
-        os.path.dirname(os.path.realpath( __file__))
-    
-)
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER']=f"{PROJECT_DIR}/tmp/media"
-app.config['MAX_CONTENT_PATH']=""
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -23,24 +19,14 @@ class NpEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return super(NpEncoder, self).default(obj)
-import json
 reader=easyocr.Reader(["ch_tra"],gpu=False)
 @app.route("/",methods = ['GET', 'POST'])
 def hello_world():
     if request.method =="POST":
-      f = request.files['file'].read()
-    #   obj = io.BytesIO()
-    #   while True:
-    #     buf = f.stream.read(1024*16)
-    #     if not buf:
-    #         break
-    #     obj(buf)
-      
-      #f.save(security.safe_join(app.config['UPLOAD_FOLDER'],f.filename))
-      result=reader.readtext(f)
+      image_bytes = request.files['file'].read()
+      result=reader.readtext(image_bytes)
       print(result)
-      
-      return jsonify(json.loads( json.dumps(result,cls=NpEncoder)))
+      return jsonify(json.loads(json.dumps(result,cls=NpEncoder)))
     html="""
     <html>
     <body>
